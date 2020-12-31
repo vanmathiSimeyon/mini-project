8e4b6243b6eeb0869834e4c32a4085e45a03f299
Vanmathi <vanmathi.ofs@gmail.com>
2020-12-31 19:57:11 +0530

    user login init

--- angular-mini-project/src/app/app.module.ts
+++ angular-mini-project/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,7 +18,8 @@ import { FileUploadComponent } from './file-upload/file-upload.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   providers: [],
